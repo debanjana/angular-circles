c3d113804056f845edc99f19a201d78b3f6379bc
debanjana <debanjana.maitra@gmail.com>
2019-04-11 19:11:45 -0400

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component , ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+  // @ViewChild ('myCanvas') canvasElement;
+  selected = [];
+  radiusValue = "50px";
+  // 'height' : 5px ; width = 5px;
+
+  // canvas setup
+  expression = {'height' : this.radiusValue , 'width' : this.radiusValue }
+
+  
+
+  ngOnInit(){
+      this.selected.length = 4;
+    this.populateCircles();
+
+  }
+
+  populateCircles(){
+    for (let count=0 ; count<this.selected.length; count++){
+      this.selected.push({
+        'id': count,
+        'isSelected': false
+      })
+    }
+  }
+
+circleClicked (element){
+  console.log(element);
+}
+
+}
+
