7bf1776bdb392e87359d71d6c0da4f2f5a449544
debanjana <debanjana.maitra@gmail.com>
2019-04-12 11:22:13 -0400

    app.component.spec.ts added

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,32 @@
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { AppComponent } from './app.component';
+
+// describe('HelloComponent', () => {
+//   let component: AppComponent;
+//   let fixture: ComponentFixture<AppComponent>;
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ AppComponent ]
+//     })
+//     .compileComponents();
+//   }));
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(AppComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+
+//  it('should decrease the  value of selected dot', () => {
+//    component.countOfCircle = 1;
+//    var event = {'keyCode' : 13};
+//    component.inputChanged(event);
+//   expect(component.noSelected).toEqual(0);
+//   });
+// });
