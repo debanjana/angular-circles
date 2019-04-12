e6b86a93ee50716cf9e3bc503af8173702d2c806
debanjana <debanjana.maitra@gmail.com>
2019-04-12 08:47:41 -0400

    on selected circles selected logic and comments added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,63 +1,70 @@
-import { Component , ViewChild} from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular';
-  // @ViewChild ('myCanvas') canvasElement;
+export class AppComponent {
   selected = [];
   countOfCircle = 4;
   noSelected = 2;
-  radiusValue = "50";
-  // 'height' : 5px ; width = 5px;
+  radiusValue = 50;
 
-  // canvas setup
-  getExpression(){
-return {'height' : this.radiusValue +'px' , 'width' : this.radiusValue +'px' }
-  }
-  
-
-  
-
-  ngOnInit(){
-    //this.selected.length = 4;
-     this.populateCircles();
-
+// method to bind the circle style with dynamic radius.
+  getExpression() {
+    return { 'height': this.radiusValue + 'px', 'width': this.radiusValue + 'px' }
   }
 
-  populateCircles(){
-    this.selected =[];
-    for (let count=0 ; count<this.countOfCircle; count++){
+
+
+
+  ngOnInit() {
+    // draw the initail circles
+    this.populateCircles();
+
+  }
+
+// method to clear rectangel and redar circles with new value.
+  populateCircles() {
+    this.selected = [];
+    for (let count = 0; count < this.countOfCircle; count++) {
       this.selected[count] = {
         'id': count,
         'isSelected': false
       };
     }
-    this.selected[this.noSelected].isSelected= true; 
+    this.selected[this.noSelected].isSelected = true;
   }
 
-circleClicked (element){
-  // clear all selections
-  this.noSelected = element.id;
-  this.populateCircles();
-  // select one
-  //this.selected[element.id].isSelected = true;
-  
-}
-inputChanged(){
-   console.log(this.noSelected , this.countOfCircle);
-  if(this.countOfCircle <= 0){
-    this.noSelected = this.noSelected;
-  } else if( this.noSelected >= this.countOfCircle){
-     console.log('yes');
-    this.noSelected = this.countOfCircle - 1;
+// method called when cicle is clicked
+  circleClicked(element) {
+    // if same circle is selected increase the radius value
+    if (this.noSelected === element.id) {
+      this.radiusValue++;
+    } else {
+      this.noSelected = element.id;
+    }
+
+    // clear all selections
+    this.populateCircles();
+
   }
 
-this.populateCircles(); 
-}
+  // method to handle input changes
+  inputChanged() {
+    // VALIDATION
+    // if number of circle is 0 the selected value with also be zero 
+    // the number of circle selected should never be greater than the total number of circles 
+    if (this.countOfCircle <= 0) {
+      this.noSelected = this.noSelected;
+    } else if (this.noSelected >= this.countOfCircle) {
+      this.noSelected = this.countOfCircle - 1;
+    }
+
+    // redraw the circles with updated value
+    this.populateCircles();
+  }
 
 }
 
